b62bc47ad19c046633d2231fa484f5cd2fcbf2e2
Saad Aziz <saadaziz1985@gmail.com>
2024-05-24 09:23:16 +0300

    image.py

--- image.py
+++ image.py
@@ -5,21 +5,17 @@ import requests
 import pandas as pd
 import  tkinter as tk
 from tkinter import filedialog
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.functions import col
+import requests
+import pandas as pd
 
-root = tk.Tk()
-root.withdraw()
-image_path = filedialog.askopenfilename()
+# Write directly to the app
+st.title(":apple: Example Streamlit App :apple:")
+st.write(
+    """Choose the fruits you want in your custome smoothie!
+    """)
 
-# Opening the image & storing it in an image object 
-img = Image.open(image_path) 
-  
-# Providing the tesseract executable 
-# location to pytesseract library 
-pytesseract.tesseract_cmd = path_to_tesseract 
-  
-# Passing the image object to image_to_string() function 
-# This function will extract the text from the image 
-text = pytesseract.image_to_string(img) 
-  
-# Displaying the extracted text 
-print(text[:-1])
+name_on_order = st.text_input('Name on Smoothie:')
+st.write('The name on your smoothie will be', name_on_order)
