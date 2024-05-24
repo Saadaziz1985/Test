cc9759a58a7039e59cc874b046cb63b1fe7db9b3
Saad Aziz <saadaziz1985@gmail.com>
2024-05-24 09:19:43 +0300

    Create image.py

---
+++ image.py
@@ -0,0 +1,19 @@
+import  tkinter as tk
+from tkinter import filedialog
+root = tk.Tk()
+root.withdraw()
+image_path = filedialog.askopenfilename()
+
+# Opening the image & storing it in an image object 
+img = Image.open(image_path) 
+  
+# Providing the tesseract executable 
+# location to pytesseract library 
+pytesseract.tesseract_cmd = path_to_tesseract 
+  
+# Passing the image object to image_to_string() function 
+# This function will extract the text from the image 
+text = pytesseract.image_to_string(img) 
+  
+# Displaying the extracted text 
+print(text[:-1])
