71ff57f814362837b23a258145322671b209b7d7
Saad Aziz <saadaziz1985@gmail.com>
2024-05-24 13:09:06 +0300

    test.py

--- test.py
+++ test.py
@@ -4,6 +4,8 @@ import requests
 import pandas as pd
 from PIL import Image 
 from pytesseract import pytesseract
+import  tkinter as tk
+from tkinter import filedialog
 
 # Write directly to the app
 st.title(":apple: Hello Example Streamlit App :apple:")
@@ -13,18 +15,3 @@ st.write(
 
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your smoothie will be', name_on_order)
-image_path = r"C:\Users\Saad Aziz\Downloads\Data Source Python\sample.png"
-
-# Opening the image & storing it in an image object 
-img = Image.open(image_path) 
-  
-# Providing the tesseract executable 
-# location to pytesseract library 
-pytesseract.tesseract_cmd = path_to_tesseract 
-  
-# Passing the image object to image_to_string() function 
-# This function will extract the text from the image 
-text = pytesseract.image_to_string(img) 
-  
-# Displaying the extracted text 
-print(text[:-1])
