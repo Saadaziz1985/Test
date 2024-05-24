01edb2745d4e23ff448b6316f90ebad35a347195
Saad Aziz <saadaziz1985@gmail.com>
2024-05-24 13:07:10 +0300

    test.py

--- test.py
+++ test.py
@@ -13,3 +13,18 @@ st.write(
 
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your smoothie will be', name_on_order)
+image_path = r"C:\Users\Saad Aziz\Downloads\Data Source Python\sample.png"
+
+# Opening the image & storing it in an image object 
+img = Image.open(image_path) 
+  
+# Providing the tesseract executable 
+# location to pytesseract library 
+pytesseract.tesseract_cmd = path_to_tesseract 
+  
+# Passing the image object to image_to_string() function 
+# This function will extract the text from the image 
+text = pytesseract.image_to_string(img) 
+  
+# Displaying the extracted text 
+print(text[:-1])
