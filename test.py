f553001f42b2ee2aca5ae74913dde91df1e69808
Saad Aziz <saadaziz1985@gmail.com>
2024-05-25 13:47:12 +0300

    test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import streamlit as st
 
-df = pd.read_csv("C:\Users\Saad Aziz\Downloads\Data Source Python\hotel_bookings.csv")  # read a CSV file inside the 'data" folder next to 'app.py'
+df = pd.read_csv(r"C:\Users\Saad Aziz\Downloads\Data Source Python\hotel_bookings.csv")  # read a CSV file inside the 'data" folder next to 'app.py'
 # df = pd.read_excel(...)  # will work for Excel files
 
 st.title("Hello world!")  # add a title
