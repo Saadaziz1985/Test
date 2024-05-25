102fa4907b8b816003e8cc40300ea77ac7933b3a
Saad Aziz <saadaziz1985@gmail.com>
2024-05-25 15:42:32 +0300

    test.py

--- test.py
+++ test.py
@@ -8,5 +8,11 @@ uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
   df = pd.read_csv(uploaded_file)
   st.write(df)
+
+  option = st.selectbox(
+...     'How would you like to be contacted?',
+...     ('Email', 'Home phone', 'Mobile phone'))
+  st.write('You selected:', option)
+
   df['Profit']=df['Profit'].sum()
   st.bar_chart(df,x="Country",y="Profit")
