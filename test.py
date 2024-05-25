e64db50c3e643d2f79a93f092941dfa38e249847
Saad Aziz <saadaziz1985@gmail.com>
2024-05-25 17:10:17 +0300

    test.py

--- test.py
+++ test.py
@@ -1,22 +1,28 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import streamlit as st
-from pytesseract import pytesseract
 
 st.title("Hello world!")
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
- 
-  #img = Image.open(image_path) 
-  
-# Providing the tesseract executable 
-# location to pytesseract library 
-  #pytesseract.tesseract_cmd = path_to_tesseract 
-  
-# Passing the image object to image_to_string() function 
-# This function will extract the text from the image 
-  text = pytesseract.image_to_string(uploaded_file) 
-  
-# Displaying the extracted text 
-  st.write(text)
+  df = pd.read_csv(uploaded_file)
+  st.write(df)
+
+  columns = st.multiselect("Columns:",df.columns)
+  filter = st.radio("Choose by:", ("inclusion","exclusion"))
+
+  if filter == "exclusion":
+      columns = [col for col in df.columns if col not in columns]
+
+  df = df[columns]
+ # x = df[df.columns[0]]
+#  y = df[df.columns[1]].sum()
+  df = df.groupby([df[df.columns[0]]])[df.columns[1]].sum()
+  st.write(df)
+ # st.write(x)
+ # st.write(y)
+
+
+  #y=df[y].sum()
+  st.bar_chart(df)
