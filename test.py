04bace305757942c6d92fa18f6521780d823e3de
Saad Aziz <saadaziz1985@gmail.com>
2024-05-25 16:26:44 +0300

    test.py

--- test.py
+++ test.py
@@ -16,7 +16,7 @@ if uploaded_file is not None:
       columns = [col for col in df.columns if col not in columns]
 
   df = df[columns]
-  x=df.loc[0]
-  y=df.loc[1]
+  x=df.iloc[0]
+  y=df.iloc[1]
   #y=df[y].sum()
   st.bar_chart(df,x=x,y=y)
