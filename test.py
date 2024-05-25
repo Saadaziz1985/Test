38fc31d67eb0b5a6bf33a20e4a28a48ea1b56ac0
Saad Aziz <saadaziz1985@gmail.com>
2024-05-25 13:06:13 +0300

    test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from io import StringIO
 
-uploaded_file = st.file_uploader("Choose a file")
+uploaded_file = st.file_uploader("Choose a file from file menu")
     # To read file as bytes:
 bytes_data = uploaded_file.getvalue()
 st.write(bytes_data)
