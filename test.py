cbe5dc72b8ca6a91a27ba1e402812c6f3e6db4bd
Saad Aziz <saadaziz1985@gmail.com>
2024-05-25 15:32:21 +0300

    test.py

--- test.py
+++ test.py
@@ -8,4 +8,5 @@ uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
   df = pd.read_csv(uploaded_file)
   st.write(df)
+  df['Profit']=df['Profit'].sum()
   st.bar_chart(df,x="Country",y="Profit")
