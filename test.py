ffe52542871c273031eb58ed21712dfb8179485a
Saad Aziz <saadaziz1985@gmail.com>
2024-05-25 13:41:21 +0300

    test.py

--- test.py
+++ test.py
@@ -4,5 +4,3 @@ from io import StringIO
 
 uploaded_file = st.file_uploader("Choose a file")
     # Can be used wherever a "file-like" object is accepted:
-dataframe = pd.read_csv(uploaded_file)
-st.write(dataframe)
