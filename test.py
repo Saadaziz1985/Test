cf51f061dee2331ab48eb8b3950260507f73d955
Saad Aziz <saadaziz1985@gmail.com>
2024-05-25 15:10:24 +0300

    test.py

--- test.py
+++ test.py
@@ -8,5 +8,4 @@ uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
   df = pd.read_csv(uploaded_file)
   st.write(df)
-  df = df.set_index("Country")
-  st.bar_chart(df)
+  st.bar_chart(df,x="Country",y="Profit")
