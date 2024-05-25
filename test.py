3faeff69108c6c07a59172630cb1f0967e6691ad
Saad Aziz <saadaziz1985@gmail.com>
2024-05-25 16:53:20 +0300

    test.py

--- test.py
+++ test.py
@@ -18,7 +18,10 @@ if uploaded_file is not None:
   df = df[columns]
   x = df[df.columns[0]]
   y = df[df.columns[1]].sum()
+  st.write(df)
+  st.write(x)
   st.write(y)
+
 
   #y=df[y].sum()
  # st.bar_chart(df,x=x,y=y)
