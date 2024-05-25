c8846ac46291435c87b79f742a204b247f5c2e18
Saad Aziz <saadaziz1985@gmail.com>
2024-05-25 13:00:33 +0300

    test.py

--- test.py
+++ test.py
@@ -1,9 +1,7 @@
 import streamlit as st
-import pandas as pd
-from io import StringIO
 
-uploaded_file = st.file_uploader("Choose a file")
-    # To read file as bytes:
-bytes_data = uploaded_file.getvalue()
-st.write(bytes_data)
-
+uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+for uploaded_file in uploaded_files:
+    bytes_data = uploaded_file.read()
+    st.write("filename:", uploaded_file.name)
+    st.write(bytes_data)
