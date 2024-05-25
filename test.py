6247103545d8015c0186d8e59bdd3f04a10cdd64
Saad Aziz <saadaziz1985@gmail.com>
2024-05-25 13:40:57 +0300

    test.py

--- test.py
+++ test.py
@@ -3,11 +3,6 @@ import pandas as pd
 from io import StringIO
 
 uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-    # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
-
     # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
+dataframe = pd.read_csv(uploaded_file)
+st.write(dataframe)
