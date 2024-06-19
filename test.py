c001d3882e3710d5e4368314c7a563c364cc26e3
Saad Aziz <saadaziz1985@gmail.com>
2024-06-19 17:00:23 +0300

    test.py

--- test.py
+++ test.py
@@ -8,26 +8,3 @@ import streamlit as st
 
 title = st.text_input("Movie title", "Life of Brian")
 st.write("The current movie title is", title)
-
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-  df = pd.read_csv(uploaded_file)
-  st.write(df)
-
-  columns = st.multiselect("Columns:",df.columns)
-  filter = st.radio("Choose by:", ("inclusion","exclusion"))
-
-  if filter == "exclusion":
-      columns = [col for col in df.columns if col not in columns]
-
-  df = df[columns]
- # x = df[df.columns[0]]
-#  y = df[df.columns[1]].sum()
-  df = df.groupby([df[df.columns[0]]])[df.columns[1]].sum()
-  st.write(df)
- # st.write(x)
- # st.write(y)
-
-
-  #y=df[y].sum()
-  st.bar_chart(df)
